9184ae7c04a4733e31fc8e93d0e806ffb0788e76
achieven <achievendar@gmail.com>
2016-11-20 16:32:23 +0200

    looks of answers is good, except color still unknown

--- client/answers.js
+++ client/answers.js
@@ -1,14 +1,57 @@
 import React from 'react'
 
 var Answers = React.createClass({
-    getInitialState: function(){
+    getInitialState: function () {
         return {
             answersText: this.props.answersText || ''
         }
     },
-    render: function(){
+    render: function () {
         return (
-            <div className="container"></div>
+            <div className="container">
+                <form action="">
+                    <div className="row">
+                        <div className="col-xs-1">
+                            <input className="form-control" name="answer" value="first" type="radio"/>
+                        </div>
+                        <div className="col-xs-11">
+                            <label className="form-control answerText">sadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfssadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadfsadf sdf asd fasd fasd fas df asdfasd fas df asd fasd fas df asdf asdf asd fasd fas df asdf asd fas df asdf asd fas df asdf asd fa sdf asd f asd f asd f asdfas df asd fas df asdf as df asd fasd fa </label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-1">
+                            <input className="form-control" name="answer" value="second" type="radio"/>
+                        </div>
+                        <div className="col-xs-11">
+                            <label className="form-control answerText">sadfsadfsadfsadf sdf asd fasd fasd fas df asdfasd fas df asd fasd fas df asdf asdf asd fasd fas df asdf asd fas df asdf asd fas df asdf asd fa sdf asd f asd f asd f asdfas df asd fas df asdf as df asd fasd fa </label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-1">
+                            <input className="form-control" name="answer" value="third" type="radio"/>
+                        </div>
+                        <div className="col-xs-11">
+                            <label className="form-control answerText">sadfsadfsadfsadf sdf asd fasd fasd fas df asdfasd fas df asd fasd fas df asdf asdf asd fasd fas df asdf asd fas df asdf asd fas df asdf asd fa sdf asd f asd f asd f asdfas df asd fas df asdf as df asd fasd fa </label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-1">
+                            <input className="form-control" name="answer" value="fourth" type="radio"/>
+                        </div>
+                        <div className="col-xs-11">
+                            <label className="form-control answerText">sadfsadfsadfsadf sdf asd fasd fasd fas df asdfasd fas df asd fasd fas df asdf asdf asd fasd fas df asdf asd fas df asdf asd fas df asdf asd fa sdf asd f asd f asd f asdfas df asd fas df asdf as df asd fasd fa </label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-1">
+                            <input className="form-control" name="answer" value="fifth" type="radio"/>
+                        </div>
+                        <div className="col-xs-11">
+                            <label className="form-control answerText">sadfsadfsadfsadf sdf asd fasd fasd fas df asdfasd fas df asd fasd fas df asdf asdf asd fasd fas df asdf asd fas df asdf asd fas df asdf asd fa sdf asd f asd f asd f asdfas df asd fas df asdf as df asd fasd fa </label>
+                        </div>
+                    </div>
+                </form>
+            </div>
         )
     }
 })
--- client/submit.js
+++ client/submit.js
@@ -4,7 +4,11 @@ var Submit = React.createClass({
     render: function(){
         return (
             <div className="container">
-                
+                <div className="row">
+                    <div className="col-xs-12 text-center">
+                        <button className="btn btn-info">Submit</button>
+                    </div>
+                </div>
             </div>
         )
     }
