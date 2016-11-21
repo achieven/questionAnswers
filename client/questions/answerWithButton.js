c981c1fcd4f1b732d82ca8883a9d1a86d6f7d5e7
achieven <achievendar@gmail.com>
2016-11-22 01:41:51 +0200

    question in middle, answers look good

--- client/questions/answerWithButton.js
+++ client/questions/answerWithButton.js
@@ -5,13 +5,15 @@ var AnswerWithButton = React.createClass({
         var inputId = "answerInput" + this.props.id
         var answerId = "answerText" + this.props.id
         return (
-            <div className="row">
-                <div className="col-xs-1">
-                    <input className="form-control" name="answer" type="radio" id={inputId}/>
-                </div>
-                <div className="col-xs-11">
-                    <label className={this.props.answerClassName}
-                           id={answerId}>{this.props.answerText}</label>
+            <div className="buttonAndAnswer">
+                <div className="row">
+                    <div className="col-xs-1">
+                        <input className="form-control answerRadioButton" name="answer" type="radio" id={inputId}/>
+                    </div>
+                    <div className="col-xs-11">
+                        <label className={this.props.answerClassName}
+                               id={answerId}>{this.props.answerText}</label>
+                    </div>
                 </div>
             </div>
         )
